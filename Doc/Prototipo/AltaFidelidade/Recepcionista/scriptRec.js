d80b00ab93fab60fb12b7df2ec9b3f65b9243588
GABRIEL ALVES RODRIGUES <0030482311028@lti.fatecso.edu.br>
2024-06-19 11:07:53 -0300

    aaaaa

--- Doc/Prototipo/AltaFidelidade/Recepcionista/scriptRec.js
+++ Doc/Prototipo/AltaFidelidade/Recepcionista/scriptRec.js
@@ -5,3 +5,15 @@ function Cadastra(){
 function Alterar(){
     alert("Alterações feitas com êxito.");
 }
+
+function Pagamento(){
+    alert("Pagamento realizado com sucesso.")
+}
+
+function Agendar(){
+    alert("Agendamento realizado com sucesso.")
+}
+
+function cancelarAgendamento(){
+    alert("Agendamento cancelado.")
+}
