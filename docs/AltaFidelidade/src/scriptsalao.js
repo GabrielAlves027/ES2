4d9cad8c43403af4097bd4d964cd3ad5958c08d3
Gabriel Alves Rodrigues <128866870+GabrielAlves027@users.noreply.github.com>
2024-06-21 20:45:32 -0300

    Update scriptsalao.js

--- docs/AltaFidelidade/src/scriptsalao.js
+++ docs/AltaFidelidade/src/scriptsalao.js
@@ -31,15 +31,15 @@ function Login(){
     const loginemail = document.getElementById("email").value;
     
     if(loginemail === "funcionario@gmail.com"){
-        window.location.href = "./Funcionario/homefuncionario.html";
+        window.location.href = "../Funcionario/homefuncionario.html";
     }
 
     if(loginemail === "recepcionista@gmail.com"){
-        window.location.href = "./Recepcionista/homerecepcionista.html";
+        window.location.href = "../Recepcionista/homerecepcionista.html";
     }
 
     if(loginemail === "cliente@gmail.com"){
-        window.location.href = "./Cliente/homecliente.html";
+        window.location.href = "../Cliente/homecliente.html";
     }
 
     if(loginemail !== "cliente@gmail.com" && loginemail !== "recepcionista@gmail.com" && loginemail !== "funcionario@gmail.com"){
