1feff0efa6f18c0e22408f5d72b553c49cf11e83
Gabriel Alves Rodrigues <128866870+GabrielAlves027@users.noreply.github.com>
2024-06-21 20:27:30 -0300

    Add files via upload

---
+++ docs/AltaFidelidade/src/scriptsalao.js
@@ -0,0 +1,52 @@
+function validateFields(){
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid && !passwordValid;
+}
+
+function isPasswordValid(){
+    const password = document.getElementById("password").value;
+    if(!password){
+        return false;
+    }
+
+    return true;
+}
+
+function isEmailValid(){
+    const email = document.getElementById("email").value;
+    if(!email){
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function validateEmail(email){
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+function Login(){
+    const loginemail = document.getElementById("email").value;
+    
+    if(loginemail === "funcionario@gmail.com"){
+        window.location.href = "./Funcionario/homefuncionario.html";
+    }
+
+    if(loginemail === "recepcionista@gmail.com"){
+        window.location.href = "./Recepcionista/homerecepcionista.html";
+    }
+
+    if(loginemail === "cliente@gmail.com"){
+        window.location.href = "./Cliente/homecliente.html";
+    }
+
+    if(loginemail !== "cliente@gmail.com" && loginemail !== "recepcionista@gmail.com" && loginemail !== "funcionario@gmail.com"){
+        alert("Essa conta é inexistente!");
+    }
+}
+
+function Cadastrar(){
+    alert("Conta criada com êxito.");
+}
