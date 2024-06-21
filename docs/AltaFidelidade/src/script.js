97600741d8ee30a17a6ac6a70eefe6ff250b70b3
Gabriel Alves Rodrigues <128866870+GabrielAlves027@users.noreply.github.com>
2024-06-21 20:39:47 -0300

    Add files via upload

---
+++ docs/AltaFidelidade/src/script.js
@@ -0,0 +1,55 @@
+function validateFields(){
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid && !passwordValid;
+}
+
+function isPasswordValid(){
+    const password = document.getElementById("password").value;
+    if(!password){
+        return false;
+    }
+
+    return true;
+}
+
+function isEmailValid(){
+    const email = document.getElementById("email").value;
+    if(!email){
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function validateEmail(email){
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+function login(){
+    const loginemail = document.getElementById("email").value;
+    
+    if(loginemail === "dono@gmail.com"){
+        window.location.href = "./Dono/homedono.html";
+    }
+
+    if(loginemail === "administrador@gmail.com"){
+        window.location.href = "./Administrador/homeadministrador.html";
+    }
+
+    if(loginemail !== "dono@gmail.com" && loginemail !== "administrador@gmail.com"){
+        alert("Essa conta é inexistente!");
+    }
+}
+
+const botao = document.querySelector(".dropbutton");
+const elementoPlataformas = document.querySelector(".dropbutton .options-drop");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
+
+function Cadastrar(){
+    alert("Conta criada com êxito.");
+}
