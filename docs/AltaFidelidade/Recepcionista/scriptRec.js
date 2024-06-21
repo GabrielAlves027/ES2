421c75f501c5248eba40d5e196f5fc041d0eb66d
Gabriel Alves Rodrigues <128866870+GabrielAlves027@users.noreply.github.com>
2024-06-21 20:16:59 -0300

    Add files via upload

---
+++ docs/AltaFidelidade/Recepcionista/scriptRec.js
@@ -0,0 +1,19 @@
+function Cadastra(){
+    alert("Cliente cadastrado com sucesso.");
+}
+
+function Alterar(){
+    alert("Alterações feitas com êxito.");
+}
+
+function Pagamento(){
+    alert("Pagamento realizado com sucesso.")
+}
+
+function Agendar(){
+    alert("Agendamento realizado com sucesso.")
+}
+
+function cancelarAgendamento(){
+    alert("Agendamento cancelado.")
+}
